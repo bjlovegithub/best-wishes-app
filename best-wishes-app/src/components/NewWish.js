1a6aa7ca477b12b9b401ac1b5e278a6c8f567ad7
billjeff <billjeff@yeah.net>
2018-04-23 22:19:38 +1000

    Style the button.

--- best-wishes-app/src/components/NewWish.js
+++ best-wishes-app/src/components/NewWish.js
@@ -3,8 +3,10 @@
 import React from 'react';
 import {
     StyleSheet, Text, View, TextInput, Picker,
-    ImageBackground, Modal, Button, Alert,
+    ImageBackground, Modal, Alert,
 } from 'react-native';
+
+import Button from 'apsl-react-native-button';
 
 import Store from '../store/Store';
 import ActionType from '../common/ActionType';
@@ -112,96 +114,109 @@ class NewWish extends React.Component {
                         fontFamily: this.state.fontFamily,
                         fontSize: this.state.fontSize,
                         color: this.state.fontColor,
-                        flex: 1,
+                        flex: 2,
                     }
                 }
-                />
-                <View style={{flexDirection: 'row', flex: 1}}>
-                  <View style={{flex: 1}}>
-                    <Text style={{textAlign: 'center'}}>Font Style</Text>
-                    <Picker
-                       selectedValue={this.state.fontFamily}
-                       itemStyle={{height: 60, fontSize: 12}}
-                       onValueChange={(itemValue, itemIndex) => this.setState({
-                           fontFamily: itemValue,
-                      })}>
-                      <Picker.Item label="Helvetica" value="Helvetica" />
-                      <Picker.Item label="Optima" value="Optima" />
-                    </Picker>
-                  </View>
-                  <View style={{flex: 1}}>
-                    <Text style={{textAlign: 'center'}}>Font Size</Text>
-                    <Picker
-                       selectedValue={this.state.fontSize.toString()}
-                       itemStyle={{height: 60, fontSize: 12}}
-                       onValueChange={(itemValue, itemIndex) => this.setState({
-                           fontSize: parseInt(itemValue),
-                      })}
-                      >
-                      <Picker.Item label="12" value="12" />
-                      <Picker.Item label="14" value="14" />
-                      <Picker.Item label="16" value="16" />
-                      <Picker.Item label="18" value="18" />
-                      <Picker.Item label="20" value="20" />
-                      <Picker.Item label="22" value="22" />
-                    </Picker>
-                  </View>
-                  <View style={{flex: 1}}>
-                    <Text style={{textAlign: 'center'}}>Font Color</Text>
-                    <Picker
-                       selectedValue={this.state.fontColor}
-                       itemStyle={{height: 60, fontSize: 12}}
-                       onValueChange={(itemValue, itemIndex) => this.setState({
-                           fontColor: itemValue,
-                      })}
-                      >
-                      <Picker.Item color='red' label={'Red'} value={'red'} />
-                      <Picker.Item color='black' label={'Black'} value={'black'} />
-                    </Picker>
-                  </View>
-                  <View style={{flex: 1}}>
-                    <Text style={{textAlign: 'center'}}>Background</Text>
-                    <Picker
-                       selectedValue={this.state.backgroundPic}
-                       itemStyle={{height: 60, fontSize: 12}}
-                       onValueChange={(itemValue, itemIndex) => this.setState({
-                           backgroundPic: itemValue,
-                      })}
-                      >
-                      <Picker.Item label={'Sea'} value={'https://images.pexels.com/photos/1562/italian-landscape-mountains-nature.jpg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'} />
-                      <Picker.Item label={'Sky'} value={'https://images.pexels.com/photos/36764/marguerite-daisy-beautiful-beauty.jpg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'} />
-                    </Picker>
-                  </View>
+              />
+              <View style={{flexDirection: 'row', flex: 1}}>
+                <View style={{flex: 1}}>
+                  <Text style={{textAlign: 'center'}}>Font Style</Text>
+                  <Picker
+                     selectedValue={this.state.fontFamily}
+                     itemStyle={{height: 60, fontSize: 12}}
+                     onValueChange={(itemValue, itemIndex) => this.setState({
+                         fontFamily: itemValue,
+                    })}>
+                    <Picker.Item label="Helvetica" value="Helvetica" />
+                    <Picker.Item label="Optima" value="Optima" />
+                  </Picker>
                 </View>
-                <View style={{flex: 3}}>
-                  <ImageBackground
-                     style={{
-                         flex: 1,
-                         justifyContent: 'center'
-                     }}
-                     source={{ uri: this.state.backgroundPic}}
-                     >
-                    <Text style={
-                              {
-                                  fontFamily: this.state.fontFamily,
-                                  fontSize: this.state.fontSize,
-                                  color: this.state.fontColor,
-                              }
-                          }>
-                      {this.state.wish}
-                    </Text>
-                  </ImageBackground>
+                <View style={{flex: 1}}>
+                  <Text style={{textAlign: 'center'}}>Font Size</Text>
+                  <Picker
+                     selectedValue={this.state.fontSize.toString()}
+                     itemStyle={{height: 60, fontSize: 12}}
+                     onValueChange={(itemValue, itemIndex) => this.setState({
+                         fontSize: parseInt(itemValue),
+                    })}
+                    >
+                    <Picker.Item label="12" value="12" />
+                    <Picker.Item label="14" value="14" />
+                    <Picker.Item label="16" value="16" />
+                    <Picker.Item label="18" value="18" />
+                    <Picker.Item label="20" value="20" />
+                    <Picker.Item label="22" value="22" />
+                  </Picker>
                 </View>
-                <View style={{flex: 3}}>
-                  <Button
-                     onPress={this.submit}
-                     title="Say to WORLD"
-                     color="#841584"
-                     />
+                <View style={{flex: 1}}>
+                  <Text style={{textAlign: 'center'}}>Font Color</Text>
+                  <Picker
+                     selectedValue={this.state.fontColor}
+                     itemStyle={{height: 60, fontSize: 12}}
+                     onValueChange={(itemValue, itemIndex) => this.setState({
+                         fontColor: itemValue,
+                    })}
+                    >
+                    <Picker.Item color='red' label={'Red'} value={'red'} />
+                    <Picker.Item color='black' label={'Black'} value={'black'} />
+                  </Picker>
                 </View>
+                <View style={{flex: 1}}>
+                  <Text style={{textAlign: 'center'}}>Background</Text>
+                  <Picker
+                     selectedValue={this.state.backgroundPic}
+                     itemStyle={{height: 60, fontSize: 12}}
+                     onValueChange={(itemValue, itemIndex) => this.setState({
+                         backgroundPic: itemValue,
+                    })}
+                    >
+                    <Picker.Item label={'Sea'} value={'https://images.pexels.com/photos/1562/italian-landscape-mountains-nature.jpg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'} />
+                    <Picker.Item label={'Sky'} value={'https://images.pexels.com/photos/36764/marguerite-daisy-beautiful-beauty.jpg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'} />
+                  </Picker>
+                </View>
+              </View>
+              <View style={{flex: 3}}>
+                <ImageBackground
+                   style={{
+                       flex: 1,
+                       justifyContent: 'center'
+                   }}
+                   source={{ uri: this.state.backgroundPic}}
+                   >
+                  <Text style={
+                            {
+                                fontFamily: this.state.fontFamily,
+                                fontSize: this.state.fontSize,
+                                color: this.state.fontColor,
+                            }
+                        }>
+                    {this.state.wish}
+                  </Text>
+                </ImageBackground>
+              </View>
+              <View style={{flex: 0.6, padding: 20, flexDirection: 'row'}}>
+                <View style={{flex: 0.3}} />
+                <Button style={styles.buttonStyle} textStyle={styles.buttonTextStyle} onPress={this.submit}>
+                  Publish To The World!
+                </Button>
+                <View style={{flex: 0.3}} />
+              </View>
             </View>
   	    );
     }
 }
+
+const styles = StyleSheet.create({
+    buttonStyle: {
+        borderColor: '#333',
+        borderWidth: 1,
+        borderRadius: 22,
+        flex: 1,
+    },
+
+    buttonTextStyle: {
+        fontSize: 12
+    },
+});
 
 module.exports = NewWish;
