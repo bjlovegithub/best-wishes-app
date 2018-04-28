8b2f7c906f4da10d6cc43ef79ad733134586f6e8
billjeff <billjeff@yeah.net>
2018-04-28 22:42:32 +1000

    Disable the nav gesture at update my wish page.

--- best-wishes-app/App.js
+++ best-wishes-app/App.js
@@ -11,33 +11,38 @@ import MyWish from './src/components/MyWish';
 import NewWish from './src/components/NewWish';
 import HomeScreen from './src/components/Home';
 
-export default StackNavigator({
-  Login: {
-    screen: Login,
-    navigationOptions: {
-      title: 'Login',
-		}
-  },
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: {
-      title: 'Best Wishes',
-		}
-  },
-  MyWish: {
-    screen: MyWish,
-    navigationOptions: {
-      title: 'My Wish',
-		}
-  },
-  NewWish: {
-    screen: NewWish,
-    navigationOptions: {
-      title: 'New Wish',
-		}
-  }
-},
+export default StackNavigator(
+    {
+        Login: {
+            screen: Login,
+            navigationOptions: {
+                title: 'Login',
+		        }
+        },
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: {
+                title: 'Best Wishes',
+		        }
+        },
+        MyWish: {
+            screen: MyWish,
+            navigationOptions: {
+                title: 'My Wish',
+		        }
+        },
+        NewWish: {
+            screen: NewWish,
+            navigationOptions: {
+                title: 'New Wish',
+		        },
+            navigationOptions: {
+                gesturesEnabled: false,
+            },
+        }
+    },
 
-{
-  initialRouteName: 'Home',
-});
+    {
+        initialRouteName: 'Home',
+    }
+);
