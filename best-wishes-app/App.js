76ac09f414fb2f9ec801d6b8a52ebf22647a451d
billjeff <billjeff@yeah.net>
2018-08-19 14:05:50 +1000

    Update main entry code.

--- best-wishes-app/App.js
+++ best-wishes-app/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 import SideMenu from 'react-native-side-menu';
 
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, NavigationActions } from 'react-navigation';
 
 import Board from './src/components/Board';
 import Login from './src/components/Login';
 import MyWish from './src/components/MyWish';
 import NewWish from './src/components/NewWish';
 import HomeScreen from './src/components/Home';
+import Actions from './src/actions/Actions';
 
 const RootStack = createStackNavigator(
   {
@@ -46,6 +47,28 @@ const RootStack = createStackNavigator(
     initialRouteName: 'Home',
   }
 );
+
+const defaultGetStateForAction = RootStack.router.getStateForAction;
+
+RootStack.router.getStateForAction = (action, state) => {
+  if (state &&
+      action.type === NavigationActions.BACK &&
+      state.routes[state.index].params &&
+      state.routes[state.index].params.isEditing)
+  {
+    console.log("have to confirm it!");
+    Alert.alert(
+      'Info',
+      'Whooooo :)',
+      [
+        {text: 'OK', onPress: () => Actions.confirmCancelEdit()},
+      ],
+      { cancelable: false }
+    );
+    return null;
+  }
+  return defaultGetStateForAction(action, state);
+};
 
 export default class App extends React.Component {
   render() {
