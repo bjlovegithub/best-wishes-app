c570d839874c74c6753010faaa55c0c612352603
billjeff <billjeff@yeah.net>
2019-05-07 20:37:47 +0800

    Update the intro sliders.

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { createStackNavigator, NavigationActions } from 'react-navigation';
 
 import AppIntroSlider from 'react-native-app-intro-slider';
 
+import { Icon } from 'react-native-elements';
+
 import Board from './src/components/Board';
 import Login from './src/components/Login';
 import MyWish from './src/components/MyWish';
@@ -14,6 +16,8 @@ import NewWish from './src/components/NewWish';
 import Feedback from './src/components/Feedback';
 import HomeScreen from './src/components/Home';
 import Actions from './src/actions/Actions';
+
+import styles from './src/styles/App';
 
 // The app uses root stack as the navigator. The stack navigator will
 // return to the previous used screen when user tries to go back.
@@ -90,23 +94,23 @@ RootStack.router.getStateForAction = (action, state) => {
 const slides = [
   {
     key: 'somethun',
-    title: 'Title 1',
-    text: 'Description.\nSay something cool',
-    image: require('./assets/create.png'),
+    title: '',
+    text: 'Share Your Wishes',
+    image: require('./assets/slide1.png'),
     backgroundColor: '#59b2ab',
   },
   {
     key: 'somethun-dos',
-    title: 'Title 2',
-    text: 'Other cool stuff',
-    image: require('./assets/create.png'),
+    title: '',
+    text: 'Right Slide To Manage\nSignin With Google Account',
+    image: require('./assets/slide2.png'),
     backgroundColor: '#febe29',
   },
   {
     key: 'somethun1',
-    title: 'Rocket guy',
-    text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
-    image: require('./assets/create.png'),
+    title: '',
+    text: 'Color Your Wish',
+    image: require('./assets/slide3.png'),
     backgroundColor: '#22bcb5',
   }
 ];
@@ -122,10 +126,14 @@ export default class App extends React.Component {
     
   _renderItem = (item) => {
     return (
-      <View>
-        <Text>{item.title}</Text>
-        <Image source={item.image} />
-        <Text>{item.text}</Text>
+      <View style={styles.slideView}>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Image source={item.image} style={styles.image} />
+        <Text></Text>
+        <Text></Text>        
+        <Text style={styles.slideText}>{item.text}</Text>
       </View>
     );
   }
@@ -135,12 +143,42 @@ export default class App extends React.Component {
     // navigation or simply by controlling state
     this.setState({ showRealApp: true });
   }
+
+  _renderNextButton = () => {
+    return (
+      <View style={styles.buttonCircle}>
+        <Icon
+          name="md-arrow-round-forward"
+          color="rgba(255, 255, 255, .9)"
+          type='ionicon'
+        />
+      </View>
+    );
+  }
+  
+  _renderDoneButton = () => {
+    return (
+      <View style={styles.buttonCircle}>
+        <Icon
+          name="md-checkmark"
+          color="rgba(255, 255, 255, .9)"
+          type='ionicon'
+        />
+      </View>
+    );
+  }  
   
   render() {
     if (this.state.showRealApp) {
       return <RootStack />;
     } else {
-      return <AppIntroSlider renderItem={this._renderItem} slides={slides} onDone={this._onDone}/>;
+      return (
+        <AppIntroSlider
+          renderItem={this._renderItem} slides={slides} onDone={this._onDone}
+          renderDoneButton={this._renderDoneButton}
+          renderNextButton={this._renderNextButton}
+          />
+      );
     }
   }  
 }
---
+++ src/styles/App.js
@@ -0,0 +1,32 @@
+import React, {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  buttonCircle: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'rgba(0, 0, 0, .2)',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  
+  image: {
+    width: 360,
+    height: 600,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  slideView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  slideText: {
+    fontFamily: 'Cochin',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'blue',
+  },
+});
